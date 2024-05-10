5e73537651b1164409a1720d786064eaaae25da6
akash02ab <akash02.ab@gmail.com>
2024-05-10 16:13:02 +0530

    typescript advanced completed

--- src/fundamentals/advanced/index.ts
+++ src/fundamentals/advanced/index.ts
@@ -108,7 +108,7 @@ function assertDate(value: unknown): asserts value is Date {
 }
 
 function loadPerson(): Person | null {
-	return null;
+	return { name: "John", dateOfBirth: new Date(1990, 1, 2) };
 }
 
 const maybePerson = loadPerson();
@@ -118,3 +118,194 @@ console.log("Name:", maybePerson.name);
 
 assertDate(maybePerson.dateOfBirth);
 console.log("Date of Birth:", maybePerson.dateOfBirth.toISOString());
+
+/*---------------------------------------------------------------*/
+
+// Function Overlading
+/*
+ * first two signatures are function overlading but will be ignored by node run time
+ * here function overloading is used to ensure that the function is called with correct arguments
+ */
+function makeDate(timestamp: number): Date;
+function makeDate(year: number, month: number, day: number): Date;
+function makeDate(timestampOrYear: number, month?: number, day?: number): Date {
+	if (month != null && day != null) {
+		return new Date(timestampOrYear, month - 1, day);
+	} else {
+		return new Date(timestampOrYear);
+	}
+}
+
+const doomsday = makeDate(1980, 1, 2); // 1 Jan 1980
+const epoch = makeDate(0); // 1 Jan 1970
+// -> const invalid = makeDate(1980, 13); // two arguments are not allowed
+console.log(doomsday.toUTCString(), epoch.toISOString());
+
+/*---------------------------------------------------------------*/
+
+// Call Signatures
+
+type Add = {
+	(a: number, b: number): number;
+	(a: number, b: number, c: number): number;
+	debugName?: string;
+};
+
+const add: Add = (a: number, b: number, c?: number) => {
+	return a + b + (c || 0);
+};
+add.debugName = "Addition Function";
+
+console.log(add(1, 2));
+console.log(add(1, 2, 3));
+
+/**
+ * Aliasing with non arrow function
+ */
+let addition: Add = function (a: number, b: number, c?: number) {
+	return a + b + (c || 0);
+};
+
+console.log(addition(1, 2));
+console.log(addition(1, 2, 3));
+
+/*---------------------------------------------------------------*/
+
+// Abstract Classes
+
+abstract class Command {
+	abstract commandLine(): string;
+
+	execute() {
+		console.log("Executing command:" + this.commandLine());
+	}
+}
+
+class GitResetCommand extends Command {
+	commandLine() {
+		return "git rest --hard";
+	}
+}
+
+class GitFetchCommand extends Command {
+	commandLine() {
+		return "git fetch --all";
+	}
+}
+
+new GitResetCommand().execute();
+new GitFetchCommand().execute();
+// -> new GitCommand().execute(); // error
+
+/*---------------------------------------------------------------*/
+
+// Index Signatures
+
+type Parson = {
+	displayName: string;
+	email: string;
+};
+
+type ParsonDictionary = {
+	[username: string]: Parson;
+};
+
+const person: ParsonDictionary = {
+	janexoxo: {
+		displayName: "Jane Doe",
+		email: "janedoe@mail.com",
+	},
+};
+
+const janexoxo = person["janexoxo"];
+console.log(janexoxo.displayName);
+
+/*---------------------------------------------------------------*/
+
+// ReadOnly Arrays and Tuples
+
+/*
+ * readonly array will not allow array to mutate
+ */
+
+function reverseSorted(input: readonly number[]): number[] {
+	return input.slice().sort().reverse();
+}
+
+const start = [1, 2, 3, 4, 5];
+const reversed = reverseSorted(start);
+console.log(start); // [1, 2, 3, 4, 5]
+console.log(reversed); // [5, 4, 3, 2, 1]
+
+type Point = readonly [number, number];
+
+function move(point: Point, x: number, y: number): Point {
+	return [point[0] + x, point[1] + y];
+}
+
+const point: Point = [0, 0];
+const moved = move(point, 10, 10);
+
+console.log(point); // [0, 0]
+console.log(moved); // [10, 10];
+
+/*---------------------------------------------------------------*/
+
+// Double Assertion
+
+const dave = {
+	name: "dave",
+	role: "drummer",
+	skills: ["jazz", "rock", "blues"],
+} as const;
+
+// -> dave.name = 'dave1'; // immutable
+
+/*--------------------------------------------------------------*/
+
+// This Parameter
+
+/*
+ * this is used for calling context in functions
+ * this is auto-populated by the runtime as functions first parameter
+ */
+
+function double(this: { value: number }) {
+	this.value = this.value * 2;
+}
+
+const valid = {
+	value: 10,
+	double,
+};
+
+valid.double();
+console.log(valid.value); // 20
+
+const invalid = {
+	valve: 10,
+	double,
+};
+
+// -> invalid.double(); // Error: Property 'value' does not exist on type '{ valve: number; double: () => void; }'.
+
+/*---------------------------------------------------------------*/
+
+// Generic Constraints
+
+type NameFields = { firstName: string; lastName: string };
+
+function addFullName<T extends NameFields>(obj: T): T & { fullName: string } {
+	return {
+		...obj,
+		fullName: obj.firstName + " " + obj.lastName,
+	};
+}
+
+const user = {
+	firstName: "John",
+	lastName: "Doe",
+};
+
+const _user = addFullName(user);
+console.log(_user.fullName);
